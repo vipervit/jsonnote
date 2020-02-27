672656c04e01bddb8e08f557cc68f4e3af225773
Viper Vit <vitolg1@gmail.com>
2020-02-26 21:51:31 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 from jsonnote import __version__
 
 with open('README.md', 'r') as f:
-    description = f.read()
+    long_description = f.read()
 
 setup(
     name='jsonnote',
@@ -11,7 +11,13 @@ setup(
     author_email='vitolg1@gmail.com',
     license='Apache',
     description='A simple utility for working with JSON files.',
-    long_description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     version=__version__,
-    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests", "test*"])
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests", "test*"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "Topic :: Utilities"
+    ]
 )
